72d2ca590eb1f2ad0b3d80ef1804577238127efb
AjayNaruka <naruka.ajay1997@gmail.com>
2021-05-05 14:54:55 +0200

    fine

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+
+const app = new Vue({
+
+  el:'#root',
+  data:{
+    autoPlayIndicator:false,
+    activeSlide:0,
+    images:['assets/img/img1.jpg','assets/img/img2.jpg','assets/img/img3.jpg','assets/img/img4.jpg']
+  },
+  methods:{
+    nextSlide(){
+      this.activeSlide++;
+      if(this.activeSlide===this.images.length) this.activeSlide=0;
+    },
+    prevSlide(){
+      this.activeSlide--;
+      if(this.activeSlide=== -1) this.activeSlide=this.images.length-1;
+    },
+    autoPlay(){
+      this.autoPlayIndicator=true
+      play = setInterval(this.nextSlide,3000)
+    },
+    autoPlayPause(){
+      clearInterval(play);
+      console.log('PAUSE');
+      this.autoPlayIndicator=false
+    }
+  },
+
+
+})
